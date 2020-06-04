7f04c850f9558d9d77db9006a3d361caf43d2983
AleRo1002 <alerori1002@gmail.com>
2020-06-03 23:35:27 -0600

    Faltan try cacth y fEstadisticas

--- GuiaNo9yNo10Sol/GuiaNo9yNo10/Program.cs
+++ GuiaNo9yNo10Sol/GuiaNo9yNo10/Program.cs
@@ -8,10 +8,7 @@ namespace GuiaNo9yNo10
 {
     class Program
     {
-        const int TAM = 7, FILAS = 7, COLUMNAS = 3;
-
-
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             /////////////////////////////////////Vectores de programa
             String[] nombres = new String[7];
@@ -28,16 +25,39 @@ namespace GuiaNo9yNo10
                 {
                     case 1: //Inicializar vectores
                         IncilizarValores(nombres, edades, esPeSa);
+                        cantPersonas = 5;
                         break;
                     case 2: //Limpiar memoria
                         LimpiezaMemoria(nombres, edades, esPeSa);
+                        cantPersonas = 0;
                         break;
                     case 3: //Ingresar datos
+                        IngresarDatos(nombres, edades, esPeSa, ref cantPersonas);
                         break;
                     case 4: //Ver listado
-                        VerListado(nombres, edades, esPeSa);
+                        if (cantPersonas >= 1)
+                        {
+                            VerListado(nombres, edades, esPeSa,cantPersonas);
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\n\t\t ...No se han encontrado datos. ");
+                            Console.WriteLine("\n\t\t ...Intente ingresando datos. ");
+                        }
+
                         break;
                     case 5: //Ver estadísticas
+                        if (cantPersonas >= 1)
+                        {
+                            // VerListado(nombres,edades,esPeSa);//
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\n\t\t ...No se han encontrado . ");
+                            Console.WriteLine("\n\t\t ...Intente ingresando datos. ");
+                        }
                         break;
                     case 6: //Salir
                         continuar = false;
@@ -69,7 +89,7 @@ namespace GuiaNo9yNo10
                     opccion = 0;
                 }
 
-            } while (opccion <= 0 || occion > 6);
+            } while (opccion <= 0 || opccion > 6);
 
         }
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -89,6 +109,8 @@ namespace GuiaNo9yNo10
                     esPeSa[j, x] = 0;
                 }
             }
+            Console.Clear();
+            Console.WriteLine("\n\t\t ...Se ha limpiado la memoria. ");
         }//LimpiezaMemoria
          ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         public static void IncilizarValores(String[] nombre, int[] edad, double[,] esPeSa)
@@ -110,17 +132,69 @@ namespace GuiaNo9yNo10
                     esPeSa[j, x] = carEsPeSa[j, x];
                 }
             }
+            Console.Clear();
+            Console.WriteLine("\n\t\t ...Vectores inicializados. ");
 
         }
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        public static void VerListado(String[] nombre, int[] edad, double[,] esPeSa)
+        //////////////////////////////inializar valores///////////////////////////////////////////////////////////////////////////////////////////////////////
+        public static void VerListado(String[] nombre, int[] edad, double[,] esPeSa,int cantPersonas)
         {
 
             Console.WriteLine("Nombres\t\t Edades\t\t Estatura\t  Peso\t\t Salario\t\t");
-            for (int z = 0; z < 5; z++)
+            for (int z = 0; z < cantPersonas ; z++)
             {
                 Console.WriteLine("{0}\t\t   {1}\t\t   {2}\t\t  {3}\t\t {4}\t\t", nombre[z], edad[z], esPeSa[z, 0], esPeSa[z, 1], esPeSa[z, 2]);
             }
+        }
+
+        public static void IngresarDatos(String[] nombre, int[] edad, double[,] esPeSa, ref int cantPersonas)
+        {
+
+            if (cantPersonas < 7)
+            {
+                //for (int i = cantPersonas; i < cantPersonas + 1; i++);
+                //for (int i = 5; i < 5 + 1; i++);
+                for (int i = cantPersonas; i < cantPersonas + 1; i++)
+                {
+                    Console.WriteLine(" Ingrese el nombre:");
+                    nombre[i] = Console.ReadLine();
+                    Console.WriteLine(" Ingrese la edad:" );
+                    edad[i] = int.Parse(Console.ReadLine());
+                }
+
+                for (int j = cantPersonas; j < cantPersonas + 1; j++)
+                {
+                    for (int x = 0; x < 3; x++)
+                    {
+                        if (x == 0)
+                        {
+                            Console.WriteLine(" Ingrese la estatura (metros):");
+                        }
+                        else
+                        {
+                            if (x == 1)
+                            {
+                                Console.WriteLine(" Ingrese el peso en (lbs):");
+                            }
+                            else
+                            {
+                                Console.WriteLine(" Ingrese el salario (dolares):");
+                            }
+                        }
+                        esPeSa[j, x] = double.Parse(Console.ReadLine());
+                    }
+                }
+                Console.Clear();
+                Console.WriteLine("\n\t\t ...Se han ingresado los datos correctamente. ");
+                cantPersonas += 1;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("\n\t\t ...Memoria llena. ");
+            }
+            //////////////////////////////////////////////////////////////////
+
         }
     }
 }
